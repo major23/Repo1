27bba2f67fb224e2fb02a8c41b510b1a111d21ac
Nikos <nikos.alexiou@upstreamsystems.com>
2016-06-29 14:21:35 +0300

    1st commit

---
+++ src/main/java/com/nikos/Pack1/Test1.java
@@ -0,0 +1,11 @@
+package com.nikos.Pack1;
+
+public class Test1 {
+	
+	public static void show(){
+		
+		System.out.println("Test1 class printing...");
+		
+	}
+
+}
---
+++ src/main/java/com/nikos/Pack2/Test2.java
@@ -0,0 +1,11 @@
+package com.nikos.Pack2;
+
+public class Test2 {
+
+	public static void show(){
+		
+		System.out.println("Test2 class printing...");
+		
+	}
+	
+}
