d418865c8189c0f271d0f2298a7e30be99fada00
Nikos <nikos.alexiou@upstreamsystems.com>
2016-06-29 14:46:14 +0300

    aaa

--- src/main/java/com/nikos/Pack1/Test1.java
+++ src/main/java/com/nikos/Pack1/Test1.java
@@ -4,8 +4,9 @@ public class Test1 {
 	
 	public static void show(){
 		
-		System.out.println("Test1 class printing...");
-		
+		System.out.println();
+		System.out.println("DEVELOP: Test1 class printing...");
+		System.out.println();
 	}
 
 }
--- src/main/java/com/nikos/Start/Start.java
+++ src/main/java/com/nikos/Start/Start.java
@@ -1,12 +1,14 @@
 package com.nikos.Start;
 
 import com.nikos.Pack1.Test1;
+import com.nikos.Pack2.Test2;
 
 public class Start {
 	
 	public static void main(String args[]){
 		
 		Test1.show();
+		Test2.show();
 		
 	}
 
