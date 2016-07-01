b4c2adc2aa0656d758535af1a6069e60f24488e5
Nikos <nikos.alexiou@upstreamsystems.com>
2016-07-01 14:43:58 +0300

    dgdfgdf

--- src/main/java/com/nikos/Start/Start.java
+++ src/main/java/com/nikos/Start/Start.java
@@ -2,6 +2,10 @@
 
 package com.nikos.Start;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.nikos.Pack1.Test1;
 import com.nikos.Pack2.Test2;
 
@@ -12,9 +16,20 @@ class Start {
 
     /**Main class.
      * @param args
+     * @throws IOException 
      */
-    public static void main(final String[] args) {
-        Test1 a = new Test1();
+    public static void main(final String[] args) throws IOException {
+    	ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream stream = classLoader.getResourceAsStream("app.properties");
+		Properties properties = new Properties();
+		properties.load(stream);
+		String s = properties.getProperty("var1");
+    	System.out.println(s);
+    	
+    	
+    	
+    	
+    	Test1 a = new Test1();
         a.show();
         Test2 b = new Test2();
         b.show();
