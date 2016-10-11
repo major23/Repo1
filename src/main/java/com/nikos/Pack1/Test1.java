f9c767642245cee6941a564b6998456ab6ecac17
Nikos Alexiou <nikosalexiou23@gmail.com>
2016-10-11 14:45:55 +0300

    vbnbvnvn

--- src/main/java/com/nikos/Pack1/Test1.java
+++ src/main/java/com/nikos/Pack1/Test1.java
@@ -10,7 +10,7 @@ public final class Test1 {
      */
     public static void show() {
         System.out.println();
-        System.out.println("DEVELOP: Test1 class printing...");
+        System.out.println("DEVELOP: Test1 class printing...UPD");
         System.out.println();
     }
 
